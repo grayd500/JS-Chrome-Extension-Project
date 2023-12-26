320a2cc3f4f5c241f7ade161ff4388fb7e93e6c5
David <grayd500@gmail.com>
2023-12-26 09:57:27 -0500

    add event listern on save input button

---
+++ index.js
@@ -0,0 +1,9 @@
+// Log out "Button clicked!" when the user clicks the "SAVE INPUT" button
+let inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function() {
+    console.log("button clicked")
+})
+
+
+
