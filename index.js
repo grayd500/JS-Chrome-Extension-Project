e68d7a227991642daa6212e5c5a4e48c24f2ea20
David <grayd500@gmail.com>
2023-12-26 21:38:06 -0500

    Update with template strings to add anchor tags for list items

--- index.js
+++ index.js
@@ -5,14 +5,21 @@ const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
+    inputEl.value = ""
     renderLeads()
 })
 
-// 1. Wrap the code below in a renderLeads() function
 function renderLeads() {
     let listItems = ""
-for (let i = 0; i < myLeads.length; i++) {
-    listItems += "<li>" + myLeads[i] + "</li>"
-}
-ulEl.innerHTML = listItems
+    for (let i = 0; i < myLeads.length; i++) {
+        // Refactor the code below to use a template string
+        listItems += `
+        <li>
+        <a target=_blank href= ${myLeads[i]}> 
+        ${myLeads[i]}
+        </a>
+        </li>
+        `
+    }
+    ulEl.innerHTML = listItems  
 }
